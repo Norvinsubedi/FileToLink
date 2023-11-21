0ef0f378116734ed80d5e27dc3bc57dd532198ab
Norvinsubedi <114564079+Norvinsubedi@users.noreply.github.com>
2023-11-22 06:27:14 +0630

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -12,14 +12,14 @@ class Var(object):
     API_ID = int(getenv('API_ID', '27078399'))
     API_HASH = str(getenv('API_HASH', '21ba7c10c1532484c2f69fdf07a99284'))
     BOT_TOKEN = str(getenv('BOT_TOKEN', ''))
-    name = str(getenv('name', 'filetolinkvjbot'))
+    name = str(getenv('name', 'filetolinkNsbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001935365147'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002132287332'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "6168162777").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5182822827").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Nash'))
